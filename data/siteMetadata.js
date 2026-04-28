a0279b67a3fb06c966924bee0ebaaf7cc62e9d19
Jasper Wu <182767243@qq.com>
2026-04-28 17:45:42 +0800

    fix: comment theme

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -71,9 +71,9 @@ const siteMetadata = {
       metadata: '0',
       // theme example: light, dark, dark_dimmed, dark_high_contrast
       // transparent_dark, preferred_color_scheme, custom
-      // theme: 'light',
+      theme: 'light',
       // theme when dark mode
-      // darkTheme: 'transparent_dark',
+      darkTheme: 'transparent_dark',
       // If the theme option above is set to 'custom`
       // please provide a link below to your custom theme css file.
       // example: https://giscus.app/themes/custom_example.css
